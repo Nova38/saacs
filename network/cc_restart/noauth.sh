9a9948f6826fb07ea17343f0815eb98eb362bef6
Thomas N Atkins <dev.csnova@gmail.com>
2023-12-29 05:09:54 -0600

    network update

--- network/cc_restart/noauth.sh
+++ network/cc_restart/noauth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
     docker stop peer0org1_noauth_ccaas
-    docker run --rm -d --name peer0org1_noauth_ccaas                      --network fabric_test                     -e CHAINCODE_SERVER_ADDRESS=0.0.0.0:9999                     -e CHAINCODE_ID=noauth_1.0.1:80d2ea0195c271b52ce4179e4c42769b0235e9df3e60d755f1e356317ab04391 -e CORE_CHAINCODE_ID_NAME=noauth_1.0.1:80d2ea0195c271b52ce4179e4c42769b0235e9df3e60d755f1e356317ab04391                     -e AUTH_MODE=noauth                     saacs_ccaas:latest
+    docker run --rm -d --name peer0org1_noauth_ccaas                      --network fabric_test                     -e CHAINCODE_SERVER_ADDRESS=0.0.0.0:9999                     -e CHAINCODE_ID=noauth_1.0.1:9f6f45eb76e8367152d74bd81a8df85db81a8d4716b0c79756279705e68e962f -e CORE_CHAINCODE_ID_NAME=noauth_1.0.1:9f6f45eb76e8367152d74bd81a8df85db81a8d4716b0c79756279705e68e962f                     -e AUTH_MODE=noauth                     saacs_ccaas:latest
 
     docker stop peer0org2_noauth_ccaas
-    docker run --rm -d --name peer0org2_noauth_ccaas                     --network fabric_test                     -e CHAINCODE_SERVER_ADDRESS=0.0.0.0:9999                     -e CHAINCODE_ID=noauth_1.0.1:80d2ea0195c271b52ce4179e4c42769b0235e9df3e60d755f1e356317ab04391 -e CORE_CHAINCODE_ID_NAME=noauth_1.0.1:80d2ea0195c271b52ce4179e4c42769b0235e9df3e60d755f1e356317ab04391                     -e AUTH_MODE=noauth                     saacs_ccaas:latest
+    docker run --rm -d --name peer0org2_noauth_ccaas                     --network fabric_test                     -e CHAINCODE_SERVER_ADDRESS=0.0.0.0:9999                     -e CHAINCODE_ID=noauth_1.0.1:9f6f45eb76e8367152d74bd81a8df85db81a8d4716b0c79756279705e68e962f -e CORE_CHAINCODE_ID_NAME=noauth_1.0.1:9f6f45eb76e8367152d74bd81a8df85db81a8d4716b0c79756279705e68e962f                     -e AUTH_MODE=noauth                     saacs_ccaas:latest
--- network/cc_restart/roles.sh
+++ network/cc_restart/roles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
     docker stop peer0org1_roles_ccaas
-    docker run --rm -d --name peer0org1_roles_ccaas                      --network fabric_test                     -e CHAINCODE_SERVER_ADDRESS=0.0.0.0:9999                     -e CHAINCODE_ID=roles_1.0.1:e844f6bb1845b27efd6d42f9f646fb09f27ea6d3de5b20098ccf8b7454a23a2e -e CORE_CHAINCODE_ID_NAME=roles_1.0.1:e844f6bb1845b27efd6d42f9f646fb09f27ea6d3de5b20098ccf8b7454a23a2e                     -e AUTH_MODE=roles                     saacs_ccaas:latest
+    docker run --rm -d --name peer0org1_roles_ccaas                      --network fabric_test                     -e CHAINCODE_SERVER_ADDRESS=0.0.0.0:9999                     -e CHAINCODE_ID=roles_1.0.1:bf087b459301f74668e4bb5726a77d9604d64d27b948ef5fb9120be2e348aace -e CORE_CHAINCODE_ID_NAME=roles_1.0.1:bf087b459301f74668e4bb5726a77d9604d64d27b948ef5fb9120be2e348aace                     -e AUTH_MODE=roles                     saacs_ccaas:latest
 
     docker stop peer0org2_roles_ccaas
-    docker run --rm -d --name peer0org2_roles_ccaas                     --network fabric_test                     -e CHAINCODE_SERVER_ADDRESS=0.0.0.0:9999                     -e CHAINCODE_ID=roles_1.0.1:e844f6bb1845b27efd6d42f9f646fb09f27ea6d3de5b20098ccf8b7454a23a2e -e CORE_CHAINCODE_ID_NAME=roles_1.0.1:e844f6bb1845b27efd6d42f9f646fb09f27ea6d3de5b20098ccf8b7454a23a2e                     -e AUTH_MODE=roles                     saacs_ccaas:latest
+    docker run --rm -d --name peer0org2_roles_ccaas                     --network fabric_test                     -e CHAINCODE_SERVER_ADDRESS=0.0.0.0:9999                     -e CHAINCODE_ID=roles_1.0.1:bf087b459301f74668e4bb5726a77d9604d64d27b948ef5fb9120be2e348aace -e CORE_CHAINCODE_ID_NAME=roles_1.0.1:bf087b459301f74668e4bb5726a77d9604d64d27b948ef5fb9120be2e348aace                     -e AUTH_MODE=roles                     saacs_ccaas:latest
